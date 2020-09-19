78aae47df53e38380d110edcc693eb870d10405f
Jodie Xiang <jodie.xiang144@gmail.com>
2020-09-18 23:42:16 -0400

    add navbar

--- twiddit/src/App.js
+++ twiddit/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NavBar from './components/NavBar.js';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
+          <NavBar/>
           <h2>Twiddit</h2>
         </div>
       </Router>
